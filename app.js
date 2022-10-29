fc65fa4e241627c6b9abfb49b4e8cb0b2182e2c8
faith <faithadetona@gmail.com>
2022-10-30 00:23:44 +0100

    'beginii'

--- app.js
+++ app.js
@@ -3,15 +3,20 @@ let taskPage = document.querySelector('.taskPage')
 let notePage = document.querySelector('.notePage')
 let note = document.querySelector('.note')
 let task = document.querySelector('.ask')
- 
-document.onreadystatechange = () => {
-    if (document.onreadystatechange = 'complete') {
+let homePageInput =  document.querySelectorAll('fieldset')
+let ADD =  document.querySelectorAll('.add div')
+let addBtn =  document.querySelectorAll('fieldset button')
+
+
       homePage.scrollIntoView(true)  
       console.log('uit')
       notePage.classList.add('none')
       taskPage.classList.add('none')
-    }
-}
+      homePageInput[0].classList.add('none')
+      homePageInput[1].classList.add('none')
+     
+    
+
 
 note.addEventListener('touchmove' ,  () =>{
  notePage.classList.remove('none')
@@ -23,3 +28,21 @@ task.addEventListener('touchmove' ,  () =>{
  taskPage.classList.remove('none')
    })
    
+   ADD[0].onclick = () => {
+    homePageInput[0].classList.remove('none')
+    document.querySelector('.add').style.filter = 'blur(10px)'
+   }
+   ADD[1].onclick = () => {
+    homePageInput[1].classList.remove('none')
+    document.querySelector('.add').style.filter = 'blur(10px)'
+   }
+
+   addBtn[0].onclick = () => {
+  let listTask =  document.createElement("span")
+  listTask.innerHTML = document.querySelector("#inputTask").value
+  document.querySelector(".taskInput").append(listTask)
+  homePageInput[0].classList.add('none')
+  
+
+
+   }
